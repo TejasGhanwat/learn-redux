ada8215d81ab27320c4077bbc1f650ecfbd0782c
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-08-03 20:58:54 -0700

    basic vounter redcer implemented

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { createStore } from "redux";
+
+//Store-> Globalized State
+
+// Action Increment
+
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// Reducer
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// Dispatch
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
